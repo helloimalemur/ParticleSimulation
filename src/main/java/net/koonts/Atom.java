174090b219d74c9a58cbda8d42bd6ac8a0c69495
James Koonts <jkoontsiii@gmail.com>
2022-09-13 20:04:37 -0400

    .

--- src/main/java/net/koonts/Atom.java
+++ src/main/java/net/koonts/Atom.java
@@ -3,11 +3,15 @@ package net.koonts;
 public class Atom {
     int x;
     int y;
+    int vx;
+    int vy;
     char c;
     Atom(int x, int y, char c) {
         this.x = x;
         this.y = y;
         this.c = c;
+        this.vx = 0;
+        this.vy = 0;
     }
 
     public Atom getAtom() {
@@ -21,6 +25,10 @@ public class Atom {
     public int getY() {
         return y;
     }
+
+    public int getVx() { return vx; }
+
+    public int getVy() { return vy; }
 
     public char getC() {
         return c;
